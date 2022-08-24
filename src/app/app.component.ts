fb5e690424480e6ec38fa0b4cd5d4231b5ebd21a
Amenyu <amenyuminnu@gmail.com>
2022-08-24 08:32:10 +0530

    add dashboard template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'TPMS';
+ 
+  sideBarOpen = true;
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
